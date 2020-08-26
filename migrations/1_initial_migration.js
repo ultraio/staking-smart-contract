829c327e28963ec8f1457cc88a55c237917feed3
bneiluj <julien.bouteloup@gmail.com>
2020-08-26 15:26:28 +0200

    modify contracts and deployments

--- migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-const Migrations = artifacts.require('./Migrations.sol');
-const Unipool = artifacts.require('./Unipool.sol');
-
-module.exports = function (deployer) {
-    deployer.deploy(Migrations);
-    deployer.deploy(Unipool);
-};
